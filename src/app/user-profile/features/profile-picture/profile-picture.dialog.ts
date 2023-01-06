c47a2453886e4c96c3dd8422238c822a04b6b109
Andrew Alderson <andrew.alderson@outlook.com>
2023-01-06 14:43:20 -0700

    feat(profile-picture): add hover and focus styles to trigger

--- src/app/user-profile/features/profile-picture/profile-picture.dialog.ts
+++ src/app/user-profile/features/profile-picture/profile-picture.dialog.ts
@@ -10,7 +10,22 @@ import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 @Component({
   selector: 'button[matx-profile-picture-dialog-trigger]',
   standalone: true,
-  template: `<ng-content select="matx-profile-picture"></ng-content>`,
+  template: `<ng-content select="matx-profile-picture"></ng-content>
+    <div class="focus-overlay">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 0 24 24"
+        width="24px"
+        fill="currentColor"
+      >
+        <path d="M0 0h24v24H0z" fill="none" />
+        <circle cx="12" cy="12" r="3.2" />
+        <path
+          d="M9 2L7.17 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2h-3.17L15 2H9zm3 15c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5z"
+        />
+      </svg>
+    </div>`,
   styles: [
     `
       :host {
@@ -31,7 +46,29 @@ import { MatDialog, MatDialogModule } from '@angular/material/dialog';
         border: 0;
         outline: 0;
         cursor: pointer;
-        background: transparent;
+        background: rgba(0, 0, 0, 0);
+        color: white;
+      }
+      .focus-overlay {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        opacity: 0;
+        background: rgba(0, 0, 0, 0.4);
+        border-radius: inherit;
+        pointer-events: none;
+        transition: opacity 0.2s ease-in-out;
+      }
+      .focus-overlay > svg {
+        opacity: 0.8;
+      }
+      :is(:host:hover, :host:focus) .focus-overlay {
+        opacity: 1;
       }
     `,
   ],
