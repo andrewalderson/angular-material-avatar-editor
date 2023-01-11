a383afc0aa902f3d4e7a5f9106209aa1d95ed274
Andrew Alderson <andrew.alderson@outlook.com>
2023-01-11 05:39:39 -0700

    chore: remove file browser component

--- storybook-e2e/src/e2e/shared/components/file-browser/file-browser.component.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('angular-material-avatar-editor', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=filebrowsercomponent--primary'));
-  it('should render the component', () => {
-    cy.get('matx-file-browser').should('exist');
-  });
-});
