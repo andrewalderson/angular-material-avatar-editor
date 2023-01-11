27c064b71987efa0f0099744bb1c9b9ab07bd0b1
Andrew Alderson <andrew.alderson@outlook.com>
2023-01-11 09:54:11 -0700

    chore: remove profile picture picker component

--- storybook-e2e/src/e2e/user-profile/features/profile-picture/profile-picture-picker.component.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('angular-material-avatar-editor', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=profilepicturepickercomponent--primary'));
-  it('should render the component', () => {
-    cy.get('matx-profile-picture-picker').should('exist');
-  });
-});
